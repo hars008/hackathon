4507a137cc1ad49e0745c78edddabd944618c6ba
hars699 <harshbansal699@gmail.com>
2023-01-22 01:30:01 +0530

    movies page cards improved

--- src/components/items/CardBoot.js
+++ src/components/items/CardBoot.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import './CardBoot.css';
+import React from "react";
+import "./CardBoot.css";
+import { useState } from "react";
 // import StarRating from 'react-star-rating';
+// import { Rating } from "@mui/material";
+
 const CardBoot = (props) => {
+  const [showCard, setShowCard] = useState(false);
   return (
     <>
-      <div className="card" style={{ width: "25rem" }}>
-        <img src={props.img} className="card-img-top" alt="..." />
+      <div
+        className="card"
+        style={{ width: "20rem" }}
+        onMouseEnter={() => setShowCard(true)}
+        onMouseLeave={() => setShowCard(false)}
+      >
+        <img src={props.img} className="card-img" alt="..." />
+        {showCard && (
+          <div className="card-img-overlay myOverlay">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">
+              <b>Genre: </b>
+              {props.genre}
+            </p>
+            <p className="card-text">
+              <b>Year of Release: </b>
+              {props.Year}
+            </p>
+            <p className="card-text">
+              <b>Rating: </b>
+              {props.rating}
+              
+               Stars
+              
+            </p>
+          </div>
+        )}
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
-          <ul className="list-group list-group-flush myCardList ">
+          {/* <ul className="list-group list-group-flush myCardList ">
             <li className="list-group-item">
               <b>genre: {props.genre}</b>
             </li>
@@ -18,22 +47,20 @@ const CardBoot = (props) => {
             <li className="list-group-item">
               <b>Rating: </b>
             </li>
-          </ul>
+          </ul> */}
         </div>
 
-        <div className="card-body">
+        {/* <div className="card-body">
           <a href="#" className="card-link">
             Card link
           </a>
           <a href="#" className="card-link">
             Another link
           </a>
-        </div>
+        </div> */}
       </div>
-
-    
     </>
   );
-}
+};
 
-export default CardBoot
+export default CardBoot;
--- src/components/pages/moviespage.js
+++ src/components/pages/moviespage.js
@@ -1,13 +1,22 @@
 import React from "react";
 import CardBoot from "../items/CardBoot";
 import "./moviespage.css"
+import NewCard from "../items/NewCard";
+import { useState } from "react";
 function Movies() {
+  const movies = [
+    {
+      cover: "https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg",
+      title: "The Shawshank Redemption",
+      releaseYear: "1994",
+    }];
+  const [showCard, setShowCard] = useState(false);
   return (
     <>
       <h1>Movies</h1>
-      
-        <h1>hello</h1>
-      
+
+      <h1>hello</h1>
+
       <div className="row">
         <CardBoot
           id="1"
@@ -28,19 +37,20 @@ function Movies() {
           img="https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg"
         />
         <CardBoot
+          id="3"
+          title="The Godfather: Part II"
+          description="The early life and career of Vito Corleone in 1920s New York City is portrayed while his son, Michael, expands and tightens his grip on the family crime syndicate"
+          img="https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg"
+        />
+        <CardBoot
           id="4"
           title="The Dark Knight"
           description="When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice"
           img="https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg"
         />
       </div>
-
-      <div className="scene">
-        <div className="card">
-          <div className="card__face card__face--front">front</div>
-          <div className="card__face card__face--back">back</div>
-        </div>
-      </div>
+      
+     
     </>
   );
 }
