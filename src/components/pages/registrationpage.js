c74041d440b7f233fbe339d98f8ac3769b5373c1
Gagan Singh <109242054+Kunal-2417@users.noreply.github.com>
2023-01-22 01:27:55 +0530

    login page (#4)

--- src/components/pages/registrationpage.js
+++ src/components/pages/registrationpage.js
@@ -1,100 +1,139 @@
 import React, { useState } from "react";
 import "./registration.css";
+import {  useNavigate } from "react-router-dom";
 
 function Registration() {
 
-  
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    showOTP: false,
-    OTP: "",
-  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [showOtp, setShowOtp] = useState(false);
+  const [otp, setotp] = useState();
+  const [id,setId]=useState();
+  const navigate = useNavigate();
+  const names = (event) => {
+    setName(event.target.value);
+  }
+  const emails = (event) => {
+    setEmail(event.target.value);
+  }
+  const passwords = (event) => {
+    setPassword(event.target.value);
+  }
+  const cpasswords = (event) => {
+    setCpassword(event.target.value);
+  }
+ const otps=(event)=>{
+  setotp(event.target.value);
+ }
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (formData.password !== formData.confirmPassword) {
-      alert("Passwords do not match!");
-    } else {
-      try {
-        // send data to server for email verification
-        // If email is verified, then show OTP input field
-        setFormData({ ...formData, showOTP: true });
-        //send email with OTP
-        alert("OTP has been sent to your email!");
-      } catch (err) {
-        console.error(err);
-        alert("An error occurred. Please try again later.");
-      }
+  const Submit = (event) => {
+    if (password !== cpassword) {
+      alert("passwords are not matching");
+      navigate("/");
     }
-  };
+    if (password.length < 8) {
+      alert("password must be of 8 characters");
+      navigate("/");
+    }
+
+    event.preventDefault();
+    const data = {
+      username: name,
+      email: email,
+      password: password,
+    }
+    fetch("http://localhost:80/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then((result) => {
+      result.json().then((resp) => {
+        if (resp) {
+          setId(resp._id);
+          setShowOtp(true);
+        }
+        else {
+          alert("Something went wrong");
+          navigate("/");
+        }
+      })
+    })
+  }
+
 
   const handleOTPSubmit = async (e) => {
     e.preventDefault();
-    try {
-      // send OTP to server for verification
-      // If OTP is verified, then show success message
-      alert("Successfully registered!");
-    } catch (err) {
-      console.error(err);
-      alert("An error occurred. Please try again later.");
+    const data = {
+      toOtp:otp,
     }
+    fetch("http://localhost:80/api/auth/verify/"+id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then((result) => {
+      result.json().then((resp) => {
+        if (resp==="ok") {
+          console.log(resp);
+        }
+        else {
+          alert("Something went wrong");
+          navigate("/");
+        }
+      })
+    })
   };
   return (
     <div className="registration-container">
       <h1>Register</h1>
-      <form onSubmit={handleSubmit} className="hi">
+      <form onSubmit={Submit}>
         <input
           type="text"
           name="name"
           placeholder="Name"
-          value={formData.name}
-          onChange={handleChange}
+          value={name}
+          onChange={names}
           required
         />
         <input
           type="email"
           name="email"
           placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
+          value={email}
+          onChange={emails}
           required
         />
         <input
           type="password"
           name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
+          placeholder="Password asli"
+          value={password}
+          onChange={passwords}
           required
         />
         <input
           type="password"
           name="confirmPassword"
           placeholder="Confirm Password"
-          value={formData.confirmPassword}
-          onChange={handleChange}
+          value={cpassword}
+          onChange={cpasswords}
           required
         />
         <button type="submit">Submit</button>
       </form>
-      {formData.showOTP && (
+      {showOtp && (
         <form onSubmit={handleOTPSubmit} className="hi">
           <input
             type="text"
             name="OTP"
             placeholder="Enter OTP sent to your email"
-            value={formData.OTP}
-            onChange={handleChange}
+            value={otp}
+            onChange={otps}
             required
           />
           <button type="submit">Verify OTP</button>
